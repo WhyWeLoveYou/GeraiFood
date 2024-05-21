2dc5da6bff939a449b96250b151329a3da282c99
arvario <berylarvariofawzi@gmail.com>
2024-05-21 15:13:53 +0700

    BBB

---
+++ app/src/main/java/com/example/geraifood/profil_page.java
@@ -0,0 +1,24 @@
+package com.example.geraifood;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class profil_page extends Fragment {
+    public profil_page() {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profil_page, container, false);
+    }
+}

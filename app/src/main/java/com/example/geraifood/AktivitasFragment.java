0f9bc70ad519d10c8fb38ee5e034ad26ecbd5a2e
thoriq <aliecha79@gmail.com>
2024-05-21 13:42:20 +0700

    eekkecing

---
+++ app/src/main/java/com/example/geraifood/AktivitasFragment.java
@@ -0,0 +1,28 @@
+package com.example.geraifood;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.geraifood.databinding.FragmentAktivitasBinding;
+
+
+public class AktivitasFragment extends Fragment {
+    private FragmentAktivitasBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentAktivitasBinding.inflate(getLayoutInflater(), container, false);
+        return binding.getRoot();
+    }
+}
